fe4608f66497d22e08c31822d25db26d550ca202
ericandoh <ohjingyu@gmail.com>
2015-04-11 15:10:30 -0700

    added some basic framework

--- ass_03.cpp
+++ ass_03.cpp
@@ -64,6 +64,17 @@ struct vec3 {
 };
 typedef struct vec3 vec3;
 
+struct color {
+  float r, g, b;
+};
+typedef struct color color;
+
+struct triangle {
+  vec3 a, b, c;
+  color* color;
+};
+typedef struct triangle triangle;
+
 //****************************************************
 // Global Variables
 //****************************************************
@@ -74,8 +85,14 @@ Viewport  viewport;
 //****************************************************
 void initScene(){
 
-  // Nothing to do here for this simple example.
+  // 3D setup - shamelessly stolen from https://www3.ntu.edu.sg/home/ehchua/programming/opengl/CG_Examples.html
 
+  glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+  glClearDepth(1.0f);
+  glEnable(GL_DEPTH_TEST);
+  glDepthFunc(GL_LEQUAL);
+  glShadeModel(GL_SMOOTH);
+  glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
 }
 
 
@@ -86,11 +103,16 @@ void myReshape(int w, int h) {
   viewport.w = w;
   viewport.h = h;
 
+  if (h == 0) {
+    h = 1;
+  }
+  GLfloat aspect = (GLfloat)w / (GLfloat)h;
+
   glViewport (0,0,viewport.w,viewport.h);
   glMatrixMode(GL_PROJECTION);
   glLoadIdentity();
-  //gluOrtho2D(0, viewport.w, 0, viewport.h);
-  gluOrtho2D(0, 1.0f, 0, 1.0f);
+
+  gluPerspective(45.0f, aspect, 0.1f, 100.0f);
 }
 
 void myKeyPressed(unsigned char key, int x, int y) {
@@ -142,18 +164,17 @@ void subtract(vec3 *dest, vec3 *a, vec3 *b) {
 }
 
 //****************************************************
-// Draw a random ass rectangle 
+// Draw a random ass triangle 
 //****************************************************
 
+void drawTriangle(triangle *triangle) {
+  glBegin(GL_TRIANGLES);
+  glColor3f(triangle->color->r, triangle->color->g, triangle->color->b);
 
-void drawRectangle(float x, float y, float width, float height, float r, float g, float b) {
-  glColor3f(r,g,b);                   // setting the color to w/e for the rect
-  glBegin(GL_POLYGON);                         // draw rectangle 
-  //glVertex3f(x val, y val, z val (won't change the point because of the projection type));
-  glVertex3f(x, y , 0.0f);               // bottom left corner of rectangle
-  glVertex3f(x, y + height, 0.0f);               // top left corner of rectangle
-  glVertex3f(x + width, y + height, 0.0f);               // top right corner of rectangle
-  glVertex3f(x + width, y, 0.0f);               // bottom right corner of rectangle
+  glVertex3f( triangle->a.x, triangle->a.y, triangle->a.z );
+  glVertex3f( triangle->b.x, triangle->b.y, triangle->b.z );
+  glVertex3f( triangle->c.x, triangle->c.y, triangle->c.z );
+
   glEnd();
 }
 
@@ -166,22 +187,40 @@ void myDisplay() {
 
   glMatrixMode(GL_MODELVIEW);             // indicate we are specifying camera transformations
   glLoadIdentity();               // make sure transformation is "zero'd"
+  gluLookAt(0.0f, 0.0f, -4.0f,     // eye position
+            0.0f, 0.0f, 0.0f,     // where to look at
+            0.0f, 1.0f, 0.0f);    // up vector
 
 
   //test draw
+  color* blue = (color*)malloc(sizeof(color));
+  blue->r = 0.0f;
+  blue->g = 0.0f;
+  blue->b = 1.0f;
 
-  glBegin(GL_QUADS);
-               glColor3f(0.5f, 0.0f, 1.0f); // make this vertex purple
-               glVertex2f(-0.75, 0.75);
-               glColor3f(1.0f, 0.0f, 0.0f); // make this vertex red
-               glVertex2f(-0.75, -0.75);
-               glColor3f(0.0f, 1.0f, 0.0f); // make this vertex green
-               glVertex2f(0.75, -0.75);
-               glColor3f(1.0f, 1.0f, 0.0f); // make this vertex yellow
-               glVertex2f(0.75, 0.75);
-  glEnd();
+  triangle* test = (triangle*)malloc(sizeof(triangle));
+
+  test->a.x = -1.0f;
+  test->a.y = 0.0f;
+  test->a.z = 0.0f;
+
+  test->b.x = 0.0f;
+  test->b.y = -1.0f;
+  test->b.z = 0.0f;
+
+  test->c.x = 0.0f;
+  test->c.y = 1.0f;
+  test->c.z = 0.0f;
+
+  test->color = blue;
+  
+  drawTriangle(test);
+
   // Start drawing
   //circle(viewport.w / 2.0 , viewport.h / 2.0 , min(viewport.w, viewport.h) * 0.45f );
+
+  free(blue);
+  free(test);
 
   glFlush();
   glutSwapBuffers();          // swap buffers (we earlier set double buffer)
